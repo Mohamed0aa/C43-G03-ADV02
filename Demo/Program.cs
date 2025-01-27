11bf02e6c4108e7172133cc4b118bc656df5fdc3
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-27 20:27:21 +0200

    list

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Collections;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 namespace Demo
 {
@@ -34,6 +35,44 @@ namespace Demo
 
 
 
+            #endregion
+
+            #region vedio 3
+            List<int> num1 = new List<int>();
+
+            Console.WriteLine($"nubers ={num.Count} sizeof ={num.Capacity}");
+            //if array_list is empty count = capacity so  it make new array with double size
+            num1.Add(1);
+            Console.WriteLine($"nubers ={num1.Count} sizeof ={num1.Capacity}");
+            num1.Add(2);
+            Console.WriteLine($"nubers ={num.Count} sizeof ={num1.Capacity}");
+            num1.AddRange(new int[] { 6, 7 });
+            Console.WriteLine($"nubers ={num1.Count} sizeof ={num1.Capacity}");
+            num1.Add(5);
+            Console.WriteLine($"nubers ={num1.Count} sizeof ={num1.Capacity}");
+            //count =capacity  allocate ne array with double size of old
+
+            //to deallocate not need memory
+            num1.TrimExcess(); //free dlete
+            Console.WriteLine($"nubers ={num.Count} sizeof ={num.Capacity}");
+
+
+            num1.EnsureCapacity(/*num*/);//set capacity with limit num 
+
+
+
+            #region method
+            List<int> number = new List<int>(){ 1,2,3,4,5};
+
+            number.BinarySearch(5);//search in list
+
+            IReadOnlyList<int> list = number.AsReadOnly();// copy list to refrence andnot allow access
+
+
+            int even_num =number.FindLast(n =>n%2==0);
+            int even_index = number.FindLastIndex(n => n % 2 == 0);
+
+            #endregion,
             #endregion
         }
     }
